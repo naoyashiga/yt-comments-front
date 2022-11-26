d4e0b9b8051e3ccdbda6ab8c46330afec86697ce
naoyashiga <>
2022-11-26 19:25:51 +0900

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "components/layouts/Footer/Footer";
 import { Nav } from "components/layouts/Nav/Nav";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-100 hover:bg-sky-800 sm:px-8 sm:py-3">
         sample button
       </button>
+      <Footer></Footer>
     </div>
   );
 }
